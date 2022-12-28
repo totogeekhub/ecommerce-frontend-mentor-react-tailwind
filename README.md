c1b2f0f7331a26e0996efda0868bac6020295744
Víctor Serrano Arriagada <victor.serrano1992@gmail.com>
2022-12-28 14:17:23 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desafio frontend Mentor 
+- [Revisa el desafio aquí] (https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6/hub)
+- [preview project]()
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MainHeader from "@/components/header/MainHeader";
+import MainProduct from "./components/product/MainProduct";
+
+const App = () => {
+  return (
+    <>
+      <MainHeader />
+      <MainProduct />
+    </>
+  );
+};
+
+export default App;
